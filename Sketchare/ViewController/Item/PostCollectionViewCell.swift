fc438267993bd7e8b159d205560243413aeee55a
ronick-grammer <glflakcm@gmail.com>
2021-08-07 17:47:09 +0900

    프로필 뷰 레이아웃 작업

---
+++ Sketchare/ViewController/Item/PostCollectionViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  PostCollectionViewCell.swift
+//  Sketchare
+//
+//  Created by RONICK on 2021/08/07.
+//
+
+import UIKit
+
+class PostCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    static let identifier = "PostCollectioViewCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    public func configure(with image: UIImage) {
+        imageView.image = image
+    }
+    
+    static func nib() -> UINib { // 셀을 컬렉션 뷰에 등록하기 위해서
+        return UINib(nibName: "PostCollectionViewCell", bundle: nil)
+    }
+
+}
--- Sketchare/ViewController/Profile/ProfileViewController.swift
+++ Sketchare/ViewController/Profile/ProfileViewController.swift
@@ -8,22 +8,56 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
+     
+    @IBOutlet var  collectionView: UICollectionView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // 셀 사이즈 세팅
+        let layout = UICollectionViewFlowLayout()
+        // UICollectionViewDelegateFlowLayout 프로토콜을 통해 사이즈를 설정해 주지 않으면 해당 세팅 사용
+        // 하지만 여기서는 프로토콜을  사용해서 사이즈 설정
+        //layout.itemSize = CGSize(width: 120, height: 120)
+        collectionView.collectionViewLayout = layout
+        
+        // 콜렉션 뷰에 셀 등록
+        collectionView.register(PostCollectionViewCell.nib(), forCellWithReuseIdentifier: PostCollectionViewCell.identifier)
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+}
 
-        // Do any additional setup after loading the view.
+// 셀을 탭하면 호출
+extension ProfileViewController: UICollectionViewDelegate { // 셀과 상호작용
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+        
+        print("you tapped me")
+    }
+}
+
+// 디스플레이할 셀의 갯수 반환
+extension ProfileViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 14
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell { //각  cell item 반환
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PostCollectionViewCell.identifier, for: indexPath) as! PostCollectionViewCell
+        
+        cell.configure(with: UIImage(named: "sketch")!)
+        
+        return cell
     }
-    */
+}
 
+// 각 셀의 크기, 마진이나 패딩 조절
+extension ProfileViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width: 120, height: 120)
+    }
 }
